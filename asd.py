0320e65afdf6b784f5840a9f853309157614e5b4
jaroscrl <jaroscrl@gspc48.informatik.uni-stuttgart.de>
2018-08-31 13:27:58 +0200

    second encounter

--- asd.py
+++ asd.py
@@ -1,5 +1,10 @@
 import sys
 import binascii
+import urllib.request
+import urllib
+import ssl
+import webbrowser
+import os
 
 from twisted.internet import reactor
 from twisted.python import log
@@ -7,6 +12,42 @@ from twisted.python import log
 from autobahn.twisted.websocket import WebSocketClientFactory, \
     WebSocketClientProtocol, \
     connectWS
+
+def openSearcher(frage,antwort1,antwort2,antwort3):
+    new=2
+    #webbrowser.get(using='google-chrome').open("https://www.google.de/search?q=" +urllib.parse.quote_plus(frage),new=new)
+    #webbrowser.get(using='google-chrome').open("https://www.google.de/search?q=" +urllib.parse.quote_plus(antwort1),new=new)
+    #webbrowser.get(using='google-chrome').open("https://www.google.de/search?q=" +urllib.parse.quote_plus(antwort2),new=new)
+    #webbrowser.get(using='google-chrome').open("https://www.google.de/search?q=" +urllib.parse.quote_plus(antwort3),new=new)
+    ctx = ssl.create_default_context()
+    ctx.check_hostname = False
+    ctx.verify_mode = ssl.CERT_NONE
+    webInput = urllib.parse.quote_plus(frage+" "+antwort1+" "+antwort2+" "+antwort3)      
+    url = "https://www.google.de/search?q=" + webInput
+    hdr = {'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.11 (KHTML, like Gecko) Chrome/23.0.1271.64 Safari/537.11',
+       'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
+       'Accept-Charset': 'ISO-8859-1,utf-8;q=0.7,*;q=0.3',
+       'Accept-Encoding': 'none',
+       'Accept-Language': 'en-US,en;q=0.8',
+       'Connection': 'keep-alive'}
+    print(url)
+    req = urllib.request.Request(url,None,hdr)
+    response = urllib.request.urlopen(req,context=ctx).read().decode('utf-8')
+    responseHigh = response
+    for wort in frage.split():
+        responseHigh = responseHigh.replace(wort,'<mark>'+wort+'</mark>')
+    for wort in antwort1.split():
+        responseHigh = responseHigh.replace(wort,'<mark>'+wort+'</mark>')
+    for wort in antwort2.split():
+        responseHigh = responseHigh.replace(wort,'<mark>'+wort+'</mark>')
+    for wort in antwort3.split():
+        responseHigh = responseHigh.replace(wort,'<mark>'+wort+'</mark>')
+    file = open('high.html','w')
+    file.write(responseHigh)
+    file.close()    
+    urlFIle = 'file://' + os.path.realpath('high.html')
+    webbrowser.get(using='google-chrome').open(urlFIle,new=new)
+    
 
 
 class EchoClientProtocol(WebSocketClientProtocol):
@@ -27,14 +68,31 @@ class EchoClientProtocol(WebSocketClientProtocol):
     def onOpen(self):
         print()
         self.sendHello()
+        self.sendCode('6c01') #GET neue Cash Termine
+        self.sendCode('6c01') #GET neue Cash Termine
+        self.sendCode('3b01') #GET links für streams
+        self.sendCode('5d010801') #GET links für streams
 
     def onMessage(self, payload, isBinary):
-        if payload[0] is 61:
+        if payload[0] is 61: # erhalten: Frage
             print(payload)
-        if (payload[0] is 94) and (len(payload) > 3):
+            offset = payload[9] - 216
+            questLen = payload[offset+15]
+            quest = payload[offset+16:offset+16+questLen].decode("utf-8")
+            ans1Len = payload[offset+17+questLen]
+            ans1 = payload[offset+18+questLen:offset+18+questLen+ans1Len].decode("utf-8")
+            ans2Len = payload[offset+19+questLen+ans1Len]
+            ans2 = payload[offset+20+questLen+ans1Len:offset+20+questLen+ans1Len+ans2Len].decode("utf-8")
+            ans3Len = payload[offset+21+questLen+ans1Len+ans2Len]
+            ans3 = payload[offset+22+questLen+ans1Len+ans2Len:offset+22+questLen+ans1Len+ans2Len+ans3Len].decode("utf-8")
+            openSearcher(quest,ans1,ans2,ans3)
+            print(payload)
+
+        if (payload[0] is 94) and (len(payload) > 3): #erhalten: Keep Alive Packet
             print("send keep alive:")
-            print(b'\x5d\x01\x08\x00'+payload[3]+b'\x01')
-            self.sendCode(b'\x5d\x01\x08\x00'+payload[3]+b'\x01')
+            newKAPacket = (int.from_bytes(b'\x5d\x01\x08\x00','big')+payload[3]+1).to_bytes(4,byteorder ='big')
+            print(newKAPacket)
+            self.sendCode(binascii.hexlify(newKAPacket))
         print(payload)
         if not isBinary:
             print("Text message received: {}".format(payload))
@@ -42,6 +100,9 @@ class EchoClientProtocol(WebSocketClientProtocol):
 
 
 if __name__ == '__main__':
+
+    #openSearcher("was ist der zweit höchster berg der welt","k2","himalja","rocky mauntains")
+    #exit()
 
     log.startLogging(sys.stdout)
 
